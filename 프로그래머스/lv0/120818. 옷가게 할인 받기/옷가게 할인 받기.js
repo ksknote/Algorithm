d59dfe064a0e46e1c7af70269377f18a25735a63
ksknote <125117987+ksknote@users.noreply.github.com>
2023-03-09 23:08:47 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,14 @@
+function solution(price) {
+    var answer = 0;
+
+    if(price >= 500000){
+        answer += parseInt(price*0.8);
+    }
+    else if(price <500000 &&price >= 300000){
+        answer += parseInt(price*0.9);
+    }
+    else if(price <300000 && price >= 100000){
+        answer += parseInt(price*0.95);
+    } else if (price <100000) {answer += price}
+    return answer;
+}
