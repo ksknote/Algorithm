5cf22af1c68f0be20ada65f67d8ece190114a1f1
ksknote <125117987+ksknote@users.noreply.github.com>
2023-03-12 22:48:23 +0900

    [level 0] Title: 대문자와 소문자, Time: 1.68 ms, Memory: 34.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,6 +1,7 @@
 function solution(my_string) {
     var answer = '';
     for (let i=0; i < my_string.length; i++){
+
         let upper = my_string.toUpperCase();
         let lower = my_string.toLowerCase();
      if (my_string.charAt(i) === upper.charAt(i)){
