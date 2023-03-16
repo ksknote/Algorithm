228196c560e8e2781ac77c133f6ad9bf81c57a24
ksknote <125117987+ksknote@users.noreply.github.com>
2023-03-16 23:28:27 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+    var answer = 0;
+    let num = numbers.replace(/zero/g, 0).replace(/one/g,1)
+    .replace(/two/g,2).replace(/three/g,3).replace(/four/g,4)
+    .replace(/five/g,5).replace(/six/g,6).replace(/seven/g,7)
+    .replace(/eight/g,8).replace(/nine/g,9);
+    answer += parseInt(num);
+    return answer;
+}
