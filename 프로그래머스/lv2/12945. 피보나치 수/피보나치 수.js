6a212cc1f0a3c8397ac4d75f86fe64e9b32e445f
ksknote <125117987+ksknote@users.noreply.github.com>
2023-03-22 00:49:13 +0900

    [level 2] Title: 피보나치 수, Time: 5.29 ms, Memory: 39 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    var answer = 0;
+    let arr = [0,1];
+    for (let i=2;i<=n;i++){
+        arr.push((arr[i-2]+arr[i-1])%1234567);
+    }
+    answer = arr[n]%1234567;
+    return answer;
+}
