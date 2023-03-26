9e94d84dea98a6d435a35227ca0738b4e1cdb94a
ksknote <125117987+ksknote@users.noreply.github.com>
2023-03-26 23:33:44 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,34 +1,30 @@
 function solution(new_id) {
     var answer = '';
-    //1단계 소문자로 치환
-    const id1 = new_id.toLowerCase()
-    //2단계 
-    const id2 = id1.replaceAll(/[^\-\_\.a-z0-9]/g, " ").replaceAll(" ","")
+    
+    //1-2단계
+    const id1_2 = new_id
+                .toLowerCase()
+                .replaceAll(/[^\.\-\_a-z0-9]/g,"")
     //3단계
-    var id3 = ""
-    for (let i=0; i<id2.length; i++) {
-        if (id2[i] === "." && id2[i] === id2[i+1]) continue;
-        id3 += id2[i];
+    let id3 = ""
+    for (let i=0; i< id1_2.length; i++) {
+        if (id1_2[i] ==="." && id1_2[i] === id1_2[i+1]) continue;
+        id3 += id1_2[i]
     }
     //4단계
-    {
-    if(id3[0] === ".") id3 = id3.substring(1);
-    if(id3[id3.length-1] === ".") id3 = id3.substring(0,id3.length-1);
-    }
+    if (id3[0] === ".") id3 = id3.substring(1);
+    if (id3[id3.length-1] === ".") id3 = id3.substring(0,id3.length-1);
     
     //5단계
     if (id3 === "") id3 += "a";
     
     //6단계
-    if (id3.length >= 16) id3 = id3.substring(0,15);
-    if(id3[id3.length-1] === ".") id3 = id3.substring(0,id3.length-1);
-
-    //7단계 
-    if(id3.length <= 2) {
-        let howmany = 3 - id3.length;
-        let repeated = id3[id3.length-1].repeat(howmany);
-        id3 += repeated;
-    }
+    if(id3.length >= 16) id3 = id3.substring(0,15);
+    if (id3[id3.length-1] === ".") id3 = id3.substring(0,id3.length-1);
     
-    return id3;
+    //7단계
+    let repeatStr = ""
+    if(id3.length <= 2) repeatStr = id3[id3.length-1].repeat(3-id3.length);
+    answer += id3 + repeatStr
+    return answer;
 }
