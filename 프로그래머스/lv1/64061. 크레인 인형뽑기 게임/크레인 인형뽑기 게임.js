d422c2c04a83f54705dfcc3e4a07881f28e46c6c
ksknote <125117987+ksknote@users.noreply.github.com>
2023-03-26 14:49:45 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.39 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,32 +1,40 @@
 function solution(board, moves) {
     var answer = 0;
-    const arr = [];
-    for (let i = 0; i< board.length; i++){
-        var arr1 = []
-        for(let j = 0; j<board.length; j++){
-        if (board[j][i] !== 0)
-        arr1.push(board[j][i]);
-    } arr.push(arr1);}
     
-    const arr2 = []
-         
+    //행 기준으로 되어있던 board 배열을 열 기준으로 변환하여 새 배열 생성
+    const board2 = [];
+    for (let i = 0; i< board.length; i++){
+        var arr1 = [] 
+        for(let j = 0; j<board.length; j++){
+        if (board[j][i] !== 0) //0은 빈칸을 의미하니 0이 아닌 값만 저장
+        arr1.push(board[j][i]); //각 행마다 배열로 담아서
+    } board2.push(arr1);} //board2에 배열 안의 배열로 push
+    
+    
+    //moves의 값을 기준으로 board2의 요소들을 바구니에 담음
+    
+    const baguni = []
+     
     moves.forEach((num) => {
-        if (arr[num-1].length>0){
-            arr2.push(arr[num-1].shift())
+        if (board2[num-1].length>0){
+            baguni.push(board2[num-1].shift())
         }
     })
-
-    let arr3 = [];
     
-    for (let n of arr2){
-        if (n === arr3[arr3.length-1]){
-            arr3.pop();
+    //바구니에서 연속된 값을 제외하고 최종 바구니에 담음
+
+    const baguni_final = [];
+    
+    for (let n of baguni){
+        if (n === baguni_final[baguni_final.length-1]){
+            baguni_final.pop();
             continue;
         }
-        arr3.push(n)
+        baguni_final.push(n)
     }
     
-    answer = arr2.length- arr3.length;
+    //첫 바구니와 최종바구니의 배열 길이의 차이가 터트려서 사라진 reuslt
+    answer = baguni.length - baguni_final.length;
     
     return answer;
 }
