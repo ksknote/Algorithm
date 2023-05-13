172d4a4acbd40fe1990fbc46e6f29ad9621b53d5
ksknote <125117987+ksknote@users.noreply.github.com>
2023-05-13 18:02:12 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,18 @@
+function solution(n) {
+    var answer = [n];
+    var num = n;
+    
+    while(num >= 1) {
+        if (num === 1) {
+            break
+        }
+        else if (num%2 ===0){
+            num = num/2;
+            answer.push(num)
+        } else {
+            num = 3*num +1;
+            answer.push(num)
+        }
+    }
+    return answer;
+}
